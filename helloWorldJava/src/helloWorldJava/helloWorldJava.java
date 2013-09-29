62192da1baa1ed510654f016a39f847424257eeb
nyhenry <zghanxiao@gmail.com>
2013-09-29 10:52:05 -0700

    create java

---
+++ helloWorldJava/src/helloWorldJava/helloWorldJava.java
@@ -0,0 +1,10 @@
+package helloWorldJava;
+
+public class helloWorldJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Java");
+	}
+
+}
